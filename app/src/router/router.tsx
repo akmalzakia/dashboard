59673f46663365c982e03990b5827bb79d5e23c4
Akmal Zaki <zaki.19051@mhs.its.ac.id>
2024-07-18 18:35:46 +0700

    init

---
+++ app/src/router/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Dashboard />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+]);
+
+export default router;
