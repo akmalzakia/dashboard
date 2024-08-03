b92106d530d08119969cea7dcc4628a570edda69
Akmal Zaki <zaki.19051@mhs.its.ac.id>
2024-08-03 20:44:36 +0700

    chore: adding react-query devtools

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,7 @@ import useTheme from './hooks/useTheme';
 import { ScreenProvider } from './context/screenContext';
 import { UserProvider } from './context/userContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ function App() {
 						<RouterProvider router={router}></RouterProvider>
 					</UserProvider>
 				</ScreenProvider>
+				<ReactQueryDevtools initialIsOpen={false} />
 			</QueryClientProvider>
 		</div>
 	);
